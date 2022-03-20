ff6cd5cdfedecbf3b949967c0f6458b3f3a459ab
Jesse <85474982+Jesse0502@users.noreply.github.com>
2022-03-20 20:02:38 +0530

    234-Palindrome-Linked-List solved

---
+++ 234-Palindrome-Linked-List.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, head) -> bool:
+        if head.next == None:
+            return True
+        a = []
+        node = head
+        while not node == None:
+            a.append(node.val)
+            node = node.next
+        
+        if not a == list(reversed(a)):
+            return False 
+        else:
+            return True
+        
+        
+class ListNode:
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+         
+a = ListNode(1,ListNode(0,ListNode(1, None)))
+
+print(Solution.isPalindrome("",a))
