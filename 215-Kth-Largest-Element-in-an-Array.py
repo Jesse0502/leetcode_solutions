61897ade1787608eb0e415ffaf7a103cafb349f2
Jesse <85474982+Jesse0502@users.noreply.github.com>
2022-04-14 09:52:54 +0530

    215-Kth-Largest-Element-in-an-Array solve

---
+++ 215-Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthLargest(self, nums, k: int) -> int:
+        nums.sort()
+        return nums[len(nums) - k]
+    
+print(   
+Solution.findKthLargest(0,[3,2,3,1,2,4,5,5,6], 4)
+)
