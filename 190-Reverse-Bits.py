82423bfd82be37e87fa39821e5ded78e2bdc0390
Jesse0502 <jas0502singh@gmail.com>
2022-05-25 13:27:41 +0530

    solved 190-Reverse-Bits.py

---
+++ 190-Reverse-Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << 31 - i)
+
+        return res
+
+
+Solution.reverseBits(Solution, 2147483648)
