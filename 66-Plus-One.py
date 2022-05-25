ce63d8df0c98541a63d1b901cb3a42687ab730e0
Jesse0502 <jas0502singh@gmail.com>
2022-05-25 12:06:51 +0530

    solved 66-Plus-One.py

---
+++ 66-Plus-One.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        inc = ""
+        for d in digits:
+            inc += str(d)
+        inc = int(inc) + 1
+        output = []
+        for i in str(inc):
+            output.append(int(i))
+        return output
+
+
+Solution.plusOne(Solution, [1, 2, 3])
